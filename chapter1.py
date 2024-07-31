33a72534aacf541a2ab282b7b49cd3e59f1b3e49
Syahrul-123 <105841100721@student.unismuh.ac.id>
2024-07-31 03:36:30 -0700

    chapter 1

---
+++ chapter1.py
@@ -0,0 +1,10 @@
+import pygame
+from PIL import Image
+import cv2
+import moviepy.editor as mp
+import pydub
+import sys
+import tkinter as tk
+
+sys.stdout.reconfigure(encoding='utf-8')
+print("✅ Semua pustaka telah terinstal dengan benar!")
